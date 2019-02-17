ff490c579a9a281b387108d8aed2ba08e530266b
Regaip KURT <regkrt@gmail.com>
2019-02-17 04:59:28 +0300

    Update ozel_program_baslangici.py

--- ozel_program_baslangici.py
+++ ozel_program_baslangici.py
@@ -1,37 +1,54 @@
+# -*- coding: utf-8 -*-
 from termcolor import cprint
 import os
 import time
+import sys
 
-def yukleme():
-    i = 1
-    while i < 10:
-        cprint("Yükleniyor.",color="green")
-        time.sleep(0.2)
-        os.system("clear")
-        cprint("Yükleniyor..",color="red")
-        time.sleep(0.2)
-        os.system("clear")
-        cprint("Yükleniyor...",color="blue")
-        time.sleep(0.2)
-        i = i + 1
-        os.system("clear")
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
+    
+class giris:
 
-def logo():
-    os.system("clear")
-    cprint(" _  _____  ____      ____   _    ____  ____  ",color="green")
-    time.sleep(0.5) 
-    cprint("| |/ / _ \|  _ \ _  |  _ \ / \  |  _ \/ ___| ",color="green")
-    time.sleep(0.5) 
-    cprint("| ' / | | | | | (_) | |_) / _ \ | |_) \___ \ ",color="green")
-    time.sleep(0.5) 
-    cprint("| . \ |_| | |_| |_  |  __/ ___ \|  _ < ___) |",color="green")
-    time.sleep(0.5) 
-    cprint("|_|\_\___/|____/(_) |_| /_/   \_\_| \_\____/ ",color="green")
-    time.sleep(0.5)
-    cprint("                                             ",color="green")
-def uyari():
-        cprint("BU PROGRAMI KULLANARAK SORUMLULUĞU ÜSTLENMİŞ OLURSUNUZ!!!\n",color="red", attrs=["bold",'underline'])
-        time.sleep(1)
-yukleme()
-logo()
-uyari()
+    @staticmethod
+    def yukleme():
+        i = 1
+        while i < 10:
+            cprint("Yükleniyor.",color="green")
+            time.sleep(0.2)
+            sys.stdout.write(CURSOR_UP_ONE)
+            sys.stdout.write(ERASE_LINE)
+            cprint("Yükleniyor..",color="red")
+            time.sleep(0.2)
+            sys.stdout.write(CURSOR_UP_ONE)
+            sys.stdout.write(ERASE_LINE)
+            cprint("Yükleniyor...",color="blue")
+            time.sleep(0.2)
+            i = i + 1
+            sys.stdout.write(CURSOR_UP_ONE)
+            sys.stdout.write(ERASE_LINE)
+
+    @staticmethod
+    def logo():
+        cprint(r" _  _____  ____      ____   _    ____  ____  ",color="green")
+        time.sleep(0.3)
+        cprint(r"| |/ / _ \|  _ \ _  |  _ \ / \  |  _ \/ ___| ",color="green")
+        time.sleep(0.3)
+        cprint(r"| ' / | | | | | (_) | |_) / _ \ | |_) \___ \ ",color="green")
+        time.sleep(0.3)
+        cprint(r"| . \ |_| | |_| |_  |  __/ ___ \|  _ < ___) |",color="green")
+        time.sleep(0.3)
+        cprint(r"|_|\_\___/|____/(_) |_| /_/   \_\_| \_\____/ ",color="green")
+        time.sleep(0.3)
+        cprint("                                             ",color="green")
+    
+    @staticmethod
+    def uyari():
+            cprint("BU PROGRAMI KULLANARAK SORUMLULUĞU ÜSTLENMİŞ OLURSUNUZ!!!\n",
+                   color="red", attrs=["bold",'underline'])
+            time.sleep(1)
+
+
+if __name__ == "__main__":
+    giris.yukleme()
+    giris.logo()
+    giris.uyari()
