b812f006e4eb70e09131293594db88248759d31c
Regaip KURT <regkrt@gmail.com>
2019-06-01 21:52:28 +0300

    android ve ios bildirim
    android-ios bildirim

--- pushover_bildirim.py
+++ pushover_bildirim.py
@@ -1,6 +1,6 @@
 from pushover import Client
 import sys
-client = Client("client", api_token="api") #client ve api yazan yerlerin değerlerini gir!
+client = Client("client", api_token="api") #client ve api yazan yerlerin değerlerini gir!!!
 a = "Bu bir deneme bildirim mesajıdır. Lütfen cevap vermeyiniz. Bu bildirimi aldıysanız programınız düzgün çalışıyor demektir. \nİyi Günler..."
 
 client.send_message(a, title="Bildirim Başarılı")
